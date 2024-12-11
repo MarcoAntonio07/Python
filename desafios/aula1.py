be42254dc9c5182da449479cd536d4effa5db56e
Marco Antonio <marcoa.amaraldasilva@gmail.com>
2024-12-10 22:23:48 -0300

    atualização

---
+++ desafios/aula1.py
@@ -0,0 +1,70 @@
+'''
+#VARIÁVEIS
+nome = 'Marco'
+idade = 24
+
+#IMPRIMIR NA TELA
+nome = input('Qual é o seu nome?')
+print('Olá', nome)
+
+#EXERCICIO
+#OBJETIVO: CALCULAR SALÁRIO HORA
+valor_salario = int(input('Qual o seu salário?'))
+horas = int(input('Quantas horas no mês?'))
+valor_hora = valor_salario / horas
+
+print('O valor do salario hora deste funcionário é {}'.format(valor_hora))
+
+
+
+#CONDICIONAIS: if(se), else(se não) e elif(se não, se)
+
+#Exemplo 1
+trabalho_terminado = True
+if trabalho_terminado == True:
+    print('Posso sair')
+else:
+    print('Não posso sair')
+
+    
+#Exemplo 2
+estar_livre = False
+
+if estar_livre == True:
+    print('Posso te ajudar')
+else:
+    print('Não posso ajudar')
+
+
+#Exemplo 3
+numero_de_atraso = 3
+
+if numero_de_atraso >= 3:
+    print('Você está suspenso')
+elif numero_de_atraso == 1:
+    print('Pode entrar, porém caso tome mais 2 faltas, irá ser suspenso')
+elif numero_de_atraso == 2:
+    print('Pode entrar, porém caso tome mais 1 falta, irá ser suspenso')
+
+
+#Exemplo 4
+primeiro_valor = 7
+segundo_valor = 7
+
+if primeiro_valor > segundo_valor:
+    print('O primeiro numero é maior')
+elif primeiro_valor < segundo_valor:
+    print('O segundo número é maior')
+else:
+    print('Os números são iguais')
+'''
+
+#Exemplo 5
+hora_do_dia = 'Manhã'
+
+if hora_do_dia == 'Manhã':
+    print('Bom dia!')
+elif hora_do_dia == 'Tarde':
+    print('Boa tarde!')
+elif hora_do_dia == 'Noite':
+    print('Boa noite!')
--- exercicios/aula4.py
+++ exercicios/aula4.py
@@ -4,3 +4,14 @@ idade = 24
 
 #Mostrar na tela
 print('Olá, eu sou o', nome, 'e tenho ', idade, 'anos')
+
+
+#--------------------------
+
+#EXERCICIO
+valor_salario = int(input('Qual o seu salário?'))
+horas = int(input('Quantas horas no mês?'))
+valor_hora = valor_salario / horas
+
+print('O valor do salario hora deste funcionário é {}'.format(valor_hora))
+
